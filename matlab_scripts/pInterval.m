b148fce64a834fa27c11a8878886de64e1303393
uyqn <s341864@oslomet.no>
2020-12-09 11:54:06 +0100

    Flere justeringer

--- matlab_scripts/pInterval.m
+++ matlab_scripts/pInterval.m
@@ -1,7 +1,7 @@
 function pInterval(estimator, n, alpha)
     p = estimator;
     z = norminv(1-alpha/2);
-    z = round(1000*z)/1000
+    z = round(1000*z)/1000;
     
     low = p - z*sqrt(p*(1-p)/n);
     high = p + z*sqrt(p*(1-p)/n);
